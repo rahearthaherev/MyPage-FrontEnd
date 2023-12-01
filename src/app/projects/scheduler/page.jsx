7402598e6056630b2bb03741a8a813a82f52dce3
wookj95 <wookj94@naver.com>
2023-12-01 17:28:04 +0900

    test push

--- src/app/projects/scheduler/page.jsx
+++ src/app/projects/scheduler/page.jsx
@@ -1,5 +1,14 @@
 "use client"
 
-export default function SchedulerPage() {
-  return <></>;
+import * as React from 'react';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+
+export default function BasicDateCalendar() {
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DateCalendar />
+    </LocalizationProvider>
+  );
 }
