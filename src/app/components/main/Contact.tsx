99184edf9ef71083cb72fa5a5ec0779c7fd25601
rahearthaherev <naverdg@gmail.com>
2023-12-23 16:17:57 +0900

    Email Copy button追加

--- src/app/components/main/Contact.tsx
+++ src/app/components/main/Contact.tsx
@@ -4,6 +4,7 @@ import { Grid, Box, Tooltip } from "@mui/material";
 import * as React from "react";
 import { HeadText, Text } from "../custom/customComponent";
 import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 export default function Contact() {
   const [copy, setCopy] = React.useState("copy");
@@ -14,22 +15,25 @@ export default function Contact() {
       <Box>
         <Text>
           E-mail Address : naverdg@gmail.com
-          {/* <Tooltip title={copy} arrow>
-            <ContentCopyIcon
-              fontSize="small"
-              sx={{
-                transition: "background-color 0.3s ease",
-                "&:hover": {
-                  backgroundColor: "rgb(230, 230, 230)",
-                  cursor: "pointer",
-                },
-              }}
-              onClick={() => {
-                navigator.clipboard.writeText("naverdg@gmail.com");
-                setCopy("copied");
-              }}
-            />
-          </Tooltip> */}
+          <CopyToClipboard
+            text="naverdg@gmail.com"
+            onCopy={() => {
+              setCopy("copied");
+            }}
+          >
+            <Tooltip title={copy} arrow>
+              <ContentCopyIcon
+                fontSize="small"
+                sx={{
+                  transition: "background-color 0.3s ease",
+                  "&:hover": {
+                    backgroundColor: "rgb(230, 230, 230)",
+                    cursor: "pointer",
+                  },
+                }}
+              />
+            </Tooltip>
+          </CopyToClipboard>
         </Text>
         <Text>Phone Number : 080 - 7432 - 1915</Text>
       </Box>
