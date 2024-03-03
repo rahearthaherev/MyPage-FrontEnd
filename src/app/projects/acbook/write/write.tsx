19d1f5d16e1276be6403e9d21cb8bbf1aad71893
rahearthaherev <naverdg@gmail.com>
2024-03-03 18:57:50 +0900

    登録と記録画面デザインかんりょう

--- src/app/projects/acbook/write/write.tsx
+++ src/app/projects/acbook/write/write.tsx
@@ -8,6 +8,7 @@ import Box from "@mui/material/Box";
 import AddBoxOutlinedIcon from "@mui/icons-material/AddBoxOutlined";
 import IndeterminateCheckBoxOutlinedIcon from "@mui/icons-material/IndeterminateCheckBoxOutlined";
 import {
+  Button,
   ButtonGroup,
   Grid,
   Input,
@@ -39,6 +40,9 @@ export default function WriteForm() {
   const [selectedAccount, setSelectedAccount] = React.useState("");
   const count = React.useRef(0);
   const [rerenderingFlag, setRerenderingFlag] = React.useState(true);
+  const [from, setFrom] = React.useState("現金");
+  const [to, setTo] = React.useState("SBJ銀行");
+  const [movingValue, setMovingValue] = React.useState(0);
   const [acBookList, setAcBookList] = React.useState<IAccountBookItem[]>([
     {
       category: "食費",
@@ -73,6 +77,18 @@ export default function WriteForm() {
     acBookList[index].description = e.target.value;
     console.log(index);
     setRerenderingFlag(!rerenderingFlag);
+  }
+
+  function handleFrom(e: any) {
+    setFrom(e.target.value);
+  }
+
+  function handleTo(e: any) {
+    setTo(e.target.value);
+  }
+
+  function HandleMovingValue(e: any) {
+    setMovingValue(e.target.value);
   }
 
   function handleAmount(e: any) {
@@ -131,38 +147,89 @@ export default function WriteForm() {
         <br />
       </Grid>
       <Grid item xs={6}>
-        <FormControl sx={{ marginBottom: "5px" }}>
-          <FormLabel id="demo-row-radio-buttons-group-label">Payment</FormLabel>
-          <RadioGroup
-            row
-            aria-labelledby="demo-row-radio-buttons-group-label"
-            name="row-radio-buttons-group"
-            value={selectedPayment}
-            onChange={handlePayment}
-          >
-            <FormControlLabel value="通帳" control={<Radio />} label="通帳" />
-            <FormControlLabel value="現金" control={<Radio />} label="現金" />
-            <FormControlLabel
-              value="クレジット"
-              control={<Radio />}
-              label="クレジット"
-            />
-          </RadioGroup>
-        </FormControl>
-        <br />
-        <FormControl sx={{ marginBottom: "5px" }}>
-          <FormLabel id="demo-row-radio-buttons-group-label">Account</FormLabel>
-          <RadioGroup
-            row
-            aria-labelledby="demo-row-radio-buttons-group-label"
-            name="row-radio-buttons-group"
-            value={selectedAccount}
-            onChange={handleAccount}
-          >
-            {selectedPayment == "現金" ? (
-              <FormControlLabel value="現金" control={<Radio />} label="現金" />
-            ) : (
-              <>
+        {selectedType == "支出" || selectedType == "輸入" ? (
+          <>
+            <FormControl sx={{ marginBottom: "5px" }}>
+              <FormLabel id="demo-row-radio-buttons-group-label">
+                Payment
+              </FormLabel>
+              <RadioGroup
+                row
+                aria-labelledby="demo-row-radio-buttons-group-label"
+                name="row-radio-buttons-group"
+                value={selectedPayment}
+                onChange={handlePayment}
+              >
+                <FormControlLabel
+                  value="通帳"
+                  control={<Radio />}
+                  label="通帳"
+                />
+                <FormControlLabel
+                  value="現金"
+                  control={<Radio />}
+                  label="現金"
+                />
+                <FormControlLabel
+                  value="クレジット"
+                  control={<Radio />}
+                  label="クレジット"
+                />
+              </RadioGroup>
+            </FormControl>
+            <br />
+            <FormControl sx={{ marginBottom: "5px" }}>
+              <FormLabel id="demo-row-radio-buttons-group-label">
+                Account
+              </FormLabel>
+              <RadioGroup
+                row
+                aria-labelledby="demo-row-radio-buttons-group-label"
+                name="row-radio-buttons-group"
+                value={selectedAccount}
+                onChange={handleAccount}
+              >
+                {selectedPayment == "現金" ? (
+                  <FormControlLabel
+                    value="現金"
+                    control={<Radio />}
+                    label="現金"
+                  />
+                ) : (
+                  <>
+                    <FormControlLabel
+                      value="三菱UFJ"
+                      control={<Radio />}
+                      label="三菱UFJ"
+                    />
+                    <FormControlLabel
+                      value="SBJ銀行"
+                      control={<Radio />}
+                      label="SBJ銀行"
+                    />
+                  </>
+                )}
+              </RadioGroup>
+            </FormControl>
+          </>
+        ) : (
+          <>
+            <FormControl sx={{ marginBottom: "5px" }}>
+              <FormLabel id="demo-row-radio-buttons-group-label">
+                From
+              </FormLabel>
+              <RadioGroup
+                row
+                aria-labelledby="demo-row-radio-buttons-group-label"
+                name="row-radio-buttons-group"
+                value={from}
+                onChange={handleFrom}
+              >
+                <FormControlLabel
+                  value="現金"
+                  control={<Radio />}
+                  label="現金"
+                />
                 <FormControlLabel
                   value="三菱UFJ"
                   control={<Radio />}
@@ -173,112 +240,166 @@ export default function WriteForm() {
                   control={<Radio />}
                   label="SBJ銀行"
                 />
-              </>
-            )}
-          </RadioGroup>
-        </FormControl>
+              </RadioGroup>
+            </FormControl>
+            <br />
+            <FormControl sx={{ marginBottom: "5px" }}>
+              <FormLabel id="demo-row-radio-buttons-group-label">To</FormLabel>
+              <RadioGroup
+                row
+                aria-labelledby="demo-row-radio-buttons-group-label"
+                name="row-radio-buttons-group"
+                value={to}
+                onChange={handleTo}
+              >
+                {selectedPayment == "現金" ? (
+                  <FormControlLabel
+                    value="現金"
+                    control={<Radio />}
+                    label="現金"
+                  />
+                ) : (
+                  <>
+                    <FormControlLabel
+                      value="三菱UFJ"
+                      control={<Radio />}
+                      label="三菱UFJ"
+                    />
+                    <FormControlLabel
+                      value="SBJ銀行"
+                      control={<Radio />}
+                      label="SBJ銀行"
+                    />
+                  </>
+                )}
+              </RadioGroup>
+            </FormControl>
+          </>
+        )}
       </Grid>
       <Grid item xs={12}>
-        <TableContainer component={Paper}>
-          <Table size="small">
-            <TableHead>
-              <TableRow>
-                <TableCell>Category</TableCell>
-                <TableCell>Description</TableCell>
-                <TableCell align="right">Amount</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {acBookList.map((item, index) => {
-                return (
-                  <TableRow key={index}>
-                    <TableCell width={80}>
-                      <Select
-                        variant="standard"
-                        id={index.toString()}
-                        label="Category"
-                        value={item.category}
-                        disableUnderline
-                        onChange={handleCategory}
-                        sx={{ width: 80 }}
-                        name={index.toString()}
-                      >
-                        <MenuItem value="食費">食費</MenuItem>
-                        <MenuItem value="保険">保険</MenuItem>
-                        <MenuItem value="ネコ">ネコ</MenuItem>
-                        <MenuItem value="趣味">趣味</MenuItem>
-                        <MenuItem value="住居">住居</MenuItem>
-                        <MenuItem value="通信">通信</MenuItem>
-                        <MenuItem value="日用">日用</MenuItem>
-                        <MenuItem value="交通">交通</MenuItem>
-                        <MenuItem value="衣類">衣類</MenuItem>
-                        <MenuItem value="文化">文化</MenuItem>
-                        <MenuItem value="美容">美容</MenuItem>
-                        <MenuItem value="医療">医療</MenuItem>
-                        <MenuItem value="その他">その他</MenuItem>
-                      </Select>
-                    </TableCell>
-                    <TableCell align="center">
-                      <TextField
-                        id={index.toString()}
-                        value={item.description}
-                        InputProps={{ disableUnderline: true }}
-                        variant="standard"
-                        onChange={handleDescription}
-                        fullWidth
-                      />
-                    </TableCell>
-                    <TableCell align="right" width={130}>
-                      <TextField
-                        id={index.toString()}
-                        value={item.amount + "円"}
-                        InputProps={{
-                          disableUnderline: true,
+        {selectedType == "支出" || selectedType == "輸入" ? (
+          <TableContainer component={Paper}>
+            <Table size="small">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Category</TableCell>
+                  <TableCell>Description</TableCell>
+                  <TableCell align="right">Amount</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {acBookList.map((item, index) => {
+                  return (
+                    <TableRow key={index}>
+                      <TableCell width={80}>
+                        <Select
+                          variant="standard"
+                          id={index.toString()}
+                          label="Category"
+                          value={item.category}
+                          disableUnderline
+                          onChange={handleCategory}
+                          sx={{ width: 80 }}
+                          name={index.toString()}
+                        >
+                          <MenuItem value="食費">食費</MenuItem>
+                          <MenuItem value="保険">保険</MenuItem>
+                          <MenuItem value="ネコ">ネコ</MenuItem>
+                          <MenuItem value="趣味">趣味</MenuItem>
+                          <MenuItem value="住居">住居</MenuItem>
+                          <MenuItem value="通信">通信</MenuItem>
+                          <MenuItem value="日用">日用</MenuItem>
+                          <MenuItem value="交通">交通</MenuItem>
+                          <MenuItem value="衣類">衣類</MenuItem>
+                          <MenuItem value="文化">文化</MenuItem>
+                          <MenuItem value="美容">美容</MenuItem>
+                          <MenuItem value="医療">医療</MenuItem>
+                          <MenuItem value="その他">その他</MenuItem>
+                        </Select>
+                      </TableCell>
+                      <TableCell align="center">
+                        <TextField
+                          id={index.toString()}
+                          value={item.description}
+                          InputProps={{ disableUnderline: true }}
+                          variant="standard"
+                          onChange={handleDescription}
+                          fullWidth
+                        />
+                      </TableCell>
+                      <TableCell align="right" width={130}>
+                        <TextField
+                          id={index.toString()}
+                          value={item.amount + "円"}
+                          InputProps={{
+                            disableUnderline: true,
+                          }}
+                          inputProps={{
+                            style: { textAlign: "right" },
+                          }}
+                          variant="standard"
+                          fullWidth
+                          onChange={handleAmount}
+                        />
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+              <TableFooter>
+                <TableRow>
+                  <TableCell></TableCell>
+                  <TableCell></TableCell>
+                  <TableCell align="right">
+                    <ButtonGroup>
+                      <AddBoxOutlinedIcon
+                        fontSize="small"
+                        sx={{
+                          "&:hover": {
+                            cursor: "pointer",
+                            backgroundColor: "rgba(0, 0, 0, 0.1)",
+                          },
                         }}
-                        inputProps={{
-                          style: { textAlign: "right" },
-                        }}
-                        variant="standard"
-                        fullWidth
-                        onChange={handleAmount}
+                        onClick={handlePlusButton}
                       />
-                    </TableCell>
-                  </TableRow>
-                );
-              })}
-            </TableBody>
-            <TableFooter>
-              <TableRow>
-                <TableCell></TableCell>
-                <TableCell></TableCell>
-                <TableCell align="right">
-                  <ButtonGroup>
-                    <AddBoxOutlinedIcon
-                      fontSize="small"
-                      sx={{
-                        "&:hover": {
-                          cursor: "pointer",
-                          backgroundColor: "rgba(0, 0, 0, 0.1)",
-                        },
-                      }}
-                      onClick={handlePlusButton}
-                    />
-                    <IndeterminateCheckBoxOutlinedIcon
-                      fontSize="small"
-                      sx={{
-                        "&:hover": {
-                          cursor: "pointer",
-                          backgroundColor: "rgba(0, 0, 0, 0.1)",
-                        },
-                      }}
-                      onClick={handleMinusButton}
-                    />
-                  </ButtonGroup>
-                </TableCell>
-              </TableRow>
-            </TableFooter>
-          </Table>
-        </TableContainer>
+                      <IndeterminateCheckBoxOutlinedIcon
+                        fontSize="small"
+                        sx={{
+                          "&:hover": {
+                            cursor: "pointer",
+                            backgroundColor: "rgba(0, 0, 0, 0.1)",
+                          },
+                        }}
+                        onClick={handleMinusButton}
+                      />
+                    </ButtonGroup>
+                  </TableCell>
+                </TableRow>
+              </TableFooter>
+            </Table>
+          </TableContainer>
+        ) : (
+          <TextField
+            label="Amount"
+            value={movingValue + "円"}
+            inputProps={{
+              style: { textAlign: "right" },
+            }}
+            variant="standard"
+            onChange={HandleMovingValue}
+          />
+        )}
+      </Grid>
+      <Grid item xs={12}>
+        <Box textAlign="right">
+          <ButtonGroup>
+            <Button variant="outlined">Submit</Button>
+            <Button variant="outlined" color="error">
+              Reset
+            </Button>
+          </ButtonGroup>
+        </Box>
       </Grid>
     </Grid>
   );
